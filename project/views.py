a4abad2b8ad344824e5993dfee4c8b4018cee376
Geopopos <groros2@icloud.com>
2020-03-14 12:31:43 -0400

    testing - Added error handling for bad form data so the user is displayed with a messaged - Added a try-except block to handle users trying to register an already existing name or email

--- project/views.py
+++ project/views.py
@@ -10,6 +10,7 @@ from flask import Flask, flash, redirect, render_template, \
         request, session, url_for
 from forms import AddTaskForm, RegisterForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 
 ################
 #### config ####
@@ -33,6 +34,19 @@ def login_required(test):
             return redirect(url_for('login'))
     return wrap   
 
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % ( \
+                getattr(form, field).label.text, error), 'error')
+
+def open_tasks():
+    return db.session.query(Task) \
+            .filter_by(status='1').order_by(Task.due_date.asc())
+
+def closed_tasks():
+    return db.session.query(Task) \
+    .filter_by(status='0').order_by(Task.due_date.asc()) 
 
 # route handlers
 @app.route('/logout/')
@@ -63,37 +77,40 @@ def login():
 @app.route('/tasks/')
 @login_required
 def tasks():
-    open_tasks = db.session.query(Task) \
-            .filter_by(status='1').order_by(Task.due_date.asc())
-    closed_tasks = db.session.query(Task) \
-            .filter_by(status='0').order_by(Task.due_date.asc())
-
     return render_template(
         'tasks.html',
         form=AddTaskForm(request.form),
-        open_tasks=open_tasks,
-        closed_tasks=closed_tasks
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
     )
 
 # Add new tasks
 @app.route('/add/', methods=['POST'])
 @login_required
 def new_task():
-        form = AddTaskForm(request.form)
-        if request.method == 'POST':
-            if form.validate_on_submit():
-                new_task = Task(
-                    form.name.data,
-                    form.due_date.data,
-                    form.priority.data,
-                    datetime.datetime.utcnow(),
-                    '1',
-                    session['user_id'] 
-                )
-                db.session.add(new_task)
-                db.session.commit()
+    error = None
+    form = AddTaskForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_task = Task(
+                form.name.data,
+                form.due_date.data,
+                form.priority.data,
+                datetime.datetime.utcnow(),
+                '1',
+                session['user_id'] 
+            )
+            db.session.add(new_task)
+            db.session.commit()
             flash('New entry was successfully posted. Thanks.')
-        return redirect(url_for('tasks'))
+            return redirect(url_for('tasks'))
+    return render_template(
+        'tasks.html',
+        form=form,
+        error=error,
+        open_tasks=open_tasks(),
+        closed_tasks=closed_tasks()
+    )
 
 # Mark tasks as complete
 @app.route('/complete/<int:task_id>/')
@@ -126,9 +143,13 @@ def register():
                 form.email.data,
                 form.password.data,
             )
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Thanks for registering. Please login.')
-            return redirect(url_for('login'))
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering. Please login.')
+                return redirect(url_for('login'))
+            except IntegrityError:
+                error = 'That username and/or email already exist.'
+                return render_template('register.html', form=form, error=error)
     return render_template('register.html', form=form, error=error)
 
