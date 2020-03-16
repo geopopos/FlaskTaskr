6176053143fd9d6b8767cbe8542900fec3a0d37e
Geopopos <groros2@icloud.com>
2020-03-16 19:36:39 -0400

    testing added original test code

--- project/test.py
+++ project/test.py
@@ -15,3 +15,29 @@ class AllTests(unittest.TestCase):
     ############################
 
     # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQlALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+                os.path.join(basedir, TEST_DB)
+        self.app = app.test_client()
+        db.create_all()
+
+    # executed after each test
+    def tearDown(self):
+           db.session.remove()
+           db.drop_all()
+
+    # each test should start with 'test'
+    def test_users_can_register(self):
+        new_user = User("michael", "michael@mherman.org", "michaelherman")
+        db.session.add(new_user)
+        db.session.commit()
+        test = db.session.query(User).all()
+        for t in test:
+            t.name
+        assert t.name == "michael"
+
+if __name__ == "__main__":
+    unittest.main()
+
